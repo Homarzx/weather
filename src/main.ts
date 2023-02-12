e5ac99c60eb5e6682d3c7fb927780006d6080413
Homar Herrera <hfherrer@espol.edu.ec>
2023-02-11 19:39:49 -0500

    servicio

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,5 @@ import { throwError } from 'rxjs';
 import { AppModule } from './app/app.module';
 
 
-if(!navigator.geolocation){
-  alert('Navegador no soporta Geolocalizacion')
-  throw new Error('Navegador no soporta la Geolocalizacion')
-}
-
-
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
